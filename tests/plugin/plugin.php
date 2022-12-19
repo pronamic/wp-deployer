5eae1b4243e251342aa3ca64d58a35324fc867d8
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-12-19 13:47:37 +0100

    Revert "v1.0.1"
    This reverts commit b0909db0cfb554e0aa760aadf0222ba8cb5b7a6f.

--- tests/plugin/plugin.php
+++ tests/plugin/plugin.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Plugin Name
  * Plugin URI:        https://example.com/plugin-name
  * Description:       Description of the plugin.
- * Version:           1.0.1
+ * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Your Name
--- tests/plugin/readme.txt
+++ tests/plugin/readme.txt
@@ -4,7 +4,7 @@ Donate link: https://example.com/
 Tags: tag1, tag2
 Requires at least: 4.7
 Tested up to: 5.4
-Stable tag: 1.0.1
+Stable tag: 4.3
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
