68de74a7e81036cfa1a30a8f038cc483d1cd0c6c
Reüel van der Steege <reuel@pronamic.nl>
2022-12-29 11:12:59 +0100

    Remove unused imports.

--- src/VersionCommand.php
+++ src/VersionCommand.php
@@ -11,15 +11,10 @@
 namespace Pronamic\Deployer;
 
 use Acme\Command\DefaultCommand;
-use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
